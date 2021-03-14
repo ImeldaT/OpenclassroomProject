10cfe58baa8c58e32b7194b4b6246151b8f2eea9
ImeldaT <80454500+ImeldaT@users.noreply.github.com>
2021-03-14 22:56:59 +0100

    Add files via upload
    Fichier HTML

---
+++ index.php
@@ -0,0 +1,14 @@
+<!doctype html>
+
+<html>
+    
+    <head>
+        <meta charset="utf-8"> 
+    </head>
+    
+    <body>
+        <p>Ceci est un exemple d'initiation &agrave; Git et GitHub.</p>
+
+    </body>
+    
+</html>
